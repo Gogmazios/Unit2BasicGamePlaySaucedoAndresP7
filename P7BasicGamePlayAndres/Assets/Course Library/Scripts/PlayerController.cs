88c22540593d402f078226e833daec2153c489d9
Gogmazios <732861@student.dusd.net>
2026-03-04 14:58:23 -0800

    Add assets
    Add dinosaurs and player.

---
+++ P7BasicGamePlayAndres/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10; 
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+
+        //keep player in bounds because reasons I guess or something like that 
+        //SPINOSAURUS 
+        if (transform.position.x < xRange) 
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z); 
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z); 
+        }
+    }
+}

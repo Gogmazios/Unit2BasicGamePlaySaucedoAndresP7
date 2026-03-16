5db9e3ca0874813fd12339611fd37fb0e406b91c
Gogmazios <732861@student.dusd.net>
2026-03-16 15:23:48 -0700

    challage 2
    C2

---
+++ P7BasicGamePlayAndres/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float startdelay = 1.0f;
+    public float time = 2f;
+    public float timee = 0f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timee)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+                timee = Time.time + time;
+            }
+        }
+
+
+
+
+
+     
+    }
+}
